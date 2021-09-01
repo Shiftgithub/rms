f6ddb0be6ddd6085c7a9528430b8db16ef9b6c0b
Shiftgithub <mhturanmohammad@gmail.com>
2021-09-01 20:06:30 +0600

    admin deleteuser details

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get("/",[HomeController::class,"index"] );
+Route::get("/users",[AdminController::class,"user"] );
+Route::get("/deleteuser/{id}",[AdminController::class,"deleteuser"] );
+
 Route::get("redirects",[HomeController::class,"redirects"] );
 
 
